fa57d127b17ce8b4472b47f78b1501a9e7dcea80
robs100DaysOfCode <robfr93@icloud.com>
2018-11-02 20:41:27 +0530

    "user and bot basics declared, now to connect botkit and recieve data"

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import ChatWindow from './components/ChatWindow';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <ChatWindow/>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -15,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/ChatWindow.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { GiftedChat  } from 'react-native-gifted-chat';
+
+
+export default class ChatWindow extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            messages: []
+        };
+
+
+    }
+
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: "This is the first step of the tutorial",
+                    createdAt: new Date(),
+                    user: {
+                        _id: 1,
+                        name: "Boto",
+                        avatar: "https://cdn-images-1.medium.com/max/1200/0*oz2e-hQtsHOWzoB4."
+                    }
+                }
+            ]
+        });
+    }
+
+    onSend(messages = []){
+        this.setState(prevState => ({
+            messages: GiftedChat.append(prevState.messages, messages),
+        }))
+    }
+
+
+	render(){
+		return(
+            <GiftedChat messages={this.state.messages} o
+            onSend={messages=>this.onSend(messages)}
+            user={{
+                _id: 3,
+                name: "Yash",
+                avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Jon_Stewart_MFF_2016.jpg/220px-Jon_Stewart_MFF_2016.jpg"
+            }}
+            showUserAvatar={true} alwaysShowSend={true} messages={this.state.messages}
+            />
+			)
+	}
+}
+
