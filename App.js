f767ffa21e7fd9e7f62f36ccb3bc6d01cc844774
robs100DaysOfCode <robfr93@icloud.com>
2018-11-02 15:59:30 +0530

    "created single page basic nav"

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home'
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Home/>
       </View>
     );
   }
@@ -19,3 +21,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default createStackNavigator({
+    First: {
+        screen: Home
+    }
+})
